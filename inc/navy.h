57aa586ea4945b465d1211c436bca96f4a710852
brice lang-nguyen <brice.lang-nguyen@epitech.eu>
2017-01-30 22:20:35 +0100

    ADD MY_PID

--- inc/navy.h
+++ inc/navy.h
@@ -5,12 +5,13 @@
 ** Login   <brice.lang-nguyen@epitech.eu>
 **
 ** Started on  Mon Jan 30 12:18:17 2017 Brice Lang-Nguyen
-** Last update Mon Jan 30 14:49:01 2017 Sadisadou
+** Last update Mon Jan 30 22:16:43 2017 Brice Lang-Nguyen
 */
 
 #ifndef NAVY_H_
 #define NAVY_H_
 
+#include <unistd.h>
 #include "lib.h"
 
 
--- src/check_map.c
+++ src/check_map.c
@@ -5,7 +5,7 @@
 ** Login   <brice.lang-nguyen@epitech.eu>
 ** 
 ** Started on  Mon Jan 30 15:58:12 2017 Brice Lang-Nguyen
-** Last update Mon Jan 30 20:02:32 2017 Brice Lang-Nguyen
+** Last update Mon Jan 30 20:35:06 2017 Brice Lang-Nguyen
 */
 
 #include "navy.h"
@@ -35,20 +35,13 @@ int	is_low_alpha(char c)
 
 int	check_map(char *buffer)
 {
-  int	i;
-
-  i = 0;
-  while (buffer[i] != '\0')
-    {
-      if (is_low_alpha(buffer[i]))
-	buffer[i] -= 32;
-      if ((i % 8 == 0 || i % 8 == 3 || i % 8 == 6) && !char_is_num(buffer[i]))
-	return (false);
-      else if ((i % 8 == 2 || i % 8 == 5) && !char_is_char(buffer[i]))
-	return (false);
-      else if ((i % 8 == 1 || i % 8 == 4) && buffer[i] != ':')
-	return (false);
-      i++;
-    }
+  /*if (is_low_alpha(buffer[i]))
+    buffer[i] -= 32;*/
+  if ((i % 8 == 0 || i % 8 == 3 || i % 8 == 6) && !char_is_num(buffer[i]))
+    return (false);
+  else if ((i % 8 == 2 || i % 8 == 5) && !char_is_char(buffer[i]))
+    return (false);
+  else if ((i % 8 == 1 || i % 8 == 4) && buffer[i] != ':')
+    return (false);
   return (true);
 }
