5cd07953bc61b11e5b630257e52acee67fa2ead2
brice lang-nguyen <brice.lang-nguyen@epitech.eu>
2017-01-30 12:50:14 +0100

    LINK NAVY.H WITH LIB.H

--- inc/navy.h
+++ inc/navy.h
@@ -5,8 +5,10 @@
 ** Login   <brice.lang-nguyen@epitech.eu>
 **
 ** Started on  Mon Jan 30 12:18:17 2017 Brice Lang-Nguyen
-** Last update Mon Jan 30 12:30:29 2017 Sadisadou
+** Last update Mon Jan 30 12:49:40 2017 Brice Lang-Nguyen
 */
+
+#include "lib.h"
 
 #ifndef NAVY_H_
 #define NAVY_H_
