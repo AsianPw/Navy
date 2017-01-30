f804fa2c4ec3f845d578f1099d026adc42d8641c
Bastien <bastien.guillaumat@epitech.eu>
2017-01-30 13:54:33 +0100

    Add news

--- main.c
+++ main.c
@@ -5,9 +5,19 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Mon Jan 30 12:32:49 2017 Sadisadou
-** Last update Mon Jan 30 12:35:46 2017 Sadisadou
+** Last update Mon Jan 30 13:00:00 2017 Sadisadou
 */
 
 #include "inc/navy.h"
 
 int	main(int ac, char** av)
+{
+  if (ac == 2 && !my_strcomp(av[1], "-h"))
+    {
+      my_printf("USAGE\n\t\   ./navy [first_player_pid] navy_positions\n\n");
+      my_printf("DESCRIPTION\n\t   first_player_pid  only for the 2nd player,\
+  pid of the first player,\n\t   navy_positions    file representing the \
+positions of the ships.\n");
+    }
+  return (0);
+}
