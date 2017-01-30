903cffdca1950d5be03112dc51fb77189eb3a54f
Bastien <bastien.guillaumat@epitech.eu>
2017-01-30 15:26:38 +0100

    Add news

---
+++ lib/my/get_next_line.h
@@ -0,0 +1,20 @@
+/*
+** get_next_line.h for get_next_line in /home/sadisadou/C-Prog_Elem/CPE_2016_getnextline
+**
+** Made by Bastien Guillaumat
+** Login   <sadisadou@epitech.net>
+**
+** Started on  Mon Jan  2 13:11:24 2017 Bastien Guillaumat
+** Last update Sun Jan 15 19:10:04 2017 Sadisadou
+*/
+
+#ifndef READ_SIZE
+#define READ_SIZE (10000)
+#endif /* !READ_SIZE*/
+
+#ifndef GET_NEXT_LINE_
+#define GET_NEXT_LINE_
+int	loop(char*);
+char*	get_next_line(const int);
+char*	get(int, int, char*, char*);
+#endif
