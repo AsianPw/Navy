83d9459be5461cee096538c435bc189d89c06bed
Bastien <bastien.guillaumat@epitech.eu>
2017-02-02 17:36:49 +0100

    advanced

--- game.c
+++ game.c
@@ -1,3 +1,4 @@
+
 /*
 ** game.c for game in /home/sadisadou/PSU/PSU_2016_navy
 **
@@ -5,7 +6,7 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Tue Jan 31 14:46:52 2017 Sadisadou
-** Last update Thu Feb  2 16:57:54 2017 Brice Lang-Nguyen
+** Last update Thu Feb  2 17:35:27 2017 Sadisadou
 */
 
 #include <signal.h>
@@ -31,28 +32,37 @@ void	handleSignal(int sig, siginfo_t* info, void* context)
   return ;
 }
 
-int	the_game()
+int	the_game(int i)
 {
   char* s;
 
-  //while (1)
-  //{
-  aff1();
-      /*while (s = get_next_line(0))
-	{
-	  if (s)
-	    my_printf("wrong position\n");
-	  else
-	    my_printf("%s: %s", s, s);
-	    }*/
-      //}
+  while (1)
+  {
+    aff1();
+    if (i == 0)
+      {
+	while (1)
+	  {
+	    my_printf("attack:  ");
+	    s = get_next_line(0);
+	    if (s)
+	      my_printf("wrong position\n");
+	    else
+	      my_printf("%s:", s, s);
+	  }
+      }
+    pause();
+  }
 }
 
 void	game1(char *buff)
 {
   struct sigaction	action;
+  int			i;
 
+  i = 0;
   my_printf("my_pid:  %d\n", getpid());
+  sigemptyset(&action.sa_mask);
   action.sa_sigaction = &handleSignal;
   action.sa_flags = SA_SIGINFO;
   my_printf("waiting for ennemy connexion...\n");
@@ -60,19 +70,22 @@ void	game1(char *buff)
   pause();
   kill(ennemy_pid(0, 1), SIGUSR1);
   my_printf("ennemy connected\n\n");
-  the_game();
+  the_game(i);
 }
 
 void	game2(char* buff, int pid1)
 {
   struct sigaction	action;
+  int			i;
 
+  i = 1;
   my_printf("my_pid:  %d\n", getpid());
+  sigemptyset(&action.sa_mask);
   action.sa_sigaction = &handleSignal;
   action.sa_flags = SA_SIGINFO;
   kill(pid1, SIGUSR1);
   sigaction(SIGUSR1, &action, NULL);
   pause();
   my_printf("successfully connected\n\n");
-  the_game();
+  the_game(i);
 }
--- src/core.c
+++ src/core.c
@@ -5,20 +5,49 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Thu Feb  2 14:40:53 2017 Sadisadou
-** Last update Thu Feb  2 14:43:59 2017 Sadisadou
+** Last update Thu Feb  2 17:22:50 2017 Sadisadou
 */
 
-#include "navy.h"
+//#include "navy.h"
+
+char	**create_map()
+{
+  char	**map;
+  int	i;
+  int	j;
+
+  i = 0;
+  map = malloc(sizeof(char*) * 9);
+  while (i < 8)
+    {
+      j = 0;
+      map[i] = malloc(sizeof(char) * 17);
+      while (j < 16)
+	{
+	  if ((j % 2) == 0)
+	    map[i][j] = '.';
+	  else
+	    map[i][j] = ' ';
+	  j++;
+	}
+      map[i][j] = '\0';
+      i++;
+    }
+  map[i] = '\0';
+  return (map);
+}
 
 void    map()
 {
   int   i;
+  char	**map;
 
+  map = create_map();
   i = 0;
   my_printf(" |A B C D E F G H\n-+---------------\n");
   while (i < 8)
     {
-      my_printf("%d|\n", i+1);
+      printf("%d|%s\n", i+1, map[i]);
       i++;
     }
 }
