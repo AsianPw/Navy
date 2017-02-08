d6f190774004c397e939ea735e4a448dab24e8d8
Bastien <bastien.guillaumat@epitech.eu>
2017-02-08 19:38:32 +0100

    check

--- src/map.c
+++ src/map.c
@@ -5,16 +5,31 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Fri Feb  3 19:12:44 2017 Sadisadou
-** Last update Sat Feb  4 00:48:00 2017 Sadisadou
+** Last update Wed Feb  8 19:38:13 2017 Sadisadou
 */
 
 #include "navy.h"
 
-char**	map_pos(char** map, char* pos)
+char**	map_pos(char** map, int	attack)
 {
-  char**	posi;
+  int	x;
+  int	y;
+  int	x1;
+  int	y1;
 
-  posi = my_str_to_wordtab(pos, ':');
-  my_printf("%s\n", posi[0]);
-  return (posi);
+  y1 = 0;
+  while (y1 < 7)
+    {
+      y = '1' + y1;
+      x1 = 0;
+      while (x1 < 8)
+	{
+	  x = 'A' + x1;
+	  if (attack == x + y)
+	    my_printf("touch\n");
+	  x1++;
+	}
+      y1++;
+    }
+  my_printf("missed\n");
 }
--- src/touch_miss.c
+++ src/touch_miss.c
@@ -5,7 +5,7 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Thu Feb  2 13:25:37 2017 Sadisadou
-** Last update Wed Feb  8 13:42:05 2017 Brice Lang-Nguyen
+** Last update Wed Feb  8 18:03:55 2017 Sadisadou
 */
 
 #include <signal.h>
@@ -26,10 +26,10 @@ void	attacker1(int state)
 	{
 	  my_printf("%s:  ", s);
 	  kill(enemy_pid(0, 1), SIGUSR1);
-	  //usleep(140000);
 	  send_coord(s);
 	  my_printf("\n\nwaiting for enemy's attack...\n");
 	  pause();
+	  receive_coord();
 	  kill(enemy_pid(0, 1), SIGUSR1);
 	  state = 0;
 	}
@@ -48,17 +48,15 @@ void	ignore(int sig, siginfo_t *info, void *context)
 
 void	attacker2(int state)
 {
-  struct sigaction	action;
   char *s;
 
-  action.sa_sigaction = &ignore;
-  action.sa_flags = SA_SIGINFO;
+  //action.sa_sigaction = &ignore;
+  //action.sa_flags = SA_SIGINFO;
   my_printf("waiting for enemy's attack...\n");
-  sigaction(SIGUSR1, &action, NULL);
-  sigaction(SIGUSR2, &action, NULL);
+  //sigaction(SIGUSR1, &action, NULL);
+  //sigaction(SIGUSR2, &action, NULL);
   pause();
   receive_coord();
-  //kill(enemy_pid(0, 1), SIGUSR1);
   while (state)
     {
       my_printf("attack:  ");
@@ -69,7 +67,8 @@ void	attacker2(int state)
 	{
 	  my_printf("%s:  \n\n", s);
 	  kill(enemy_pid(0, 1), SIGUSR1);
-	  pause();
+	  send_coord(s);
+	  //pause();
 	  state = 0;
 	}
       free(s);
