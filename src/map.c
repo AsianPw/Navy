75f3d216479fe590753ce335ec6067e6c5782c21
Bastien <bastien.guillaumat@epitech.eu>
2017-02-09 17:18:51 +0100

    zdzs

--- src/map.c
+++ src/map.c
@@ -5,7 +5,7 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Fri Feb  3 19:12:44 2017 Sadisadou
-** Last update Thu Feb  9 16:14:59 2017 Sadisadou
+** Last update Thu Feb  9 17:18:49 2017 Sadisadou
 */
 
 #include "navy.h"
@@ -31,7 +31,7 @@ char**	map_pos(char** map, int	attack)
 		kill(enemy_pid(0, 1), SIGUSR1);
 	      else
 		kill(enemy_pid(0, 1), SIGUSR2);
-	      return (0);
+	      return (NULL);
 	    }
 	  x1++;
 	}
