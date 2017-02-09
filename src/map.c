e59a1d08148a79917df34adeed35138c643c1521
Bastien <bastien.guillaumat@epitech.eu>
2017-02-09 16:15:06 +0100

    send_nudes

--- src/map.c
+++ src/map.c
@@ -5,7 +5,7 @@
 ** Login   <bastien.guillaumat@epitech.eu@epitech.net>
 **
 ** Started on  Fri Feb  3 19:12:44 2017 Sadisadou
-** Last update Wed Feb  8 19:38:13 2017 Sadisadou
+** Last update Thu Feb  9 16:14:59 2017 Sadisadou
 */
 
 #include "navy.h"
@@ -25,11 +25,16 @@ char**	map_pos(char** map, int	attack)
       while (x1 < 8)
 	{
 	  x = 'A' + x1;
-	  if (attack == x + y)
-	    my_printf("touch\n");
+	  if (attack == (x + y))
+	    {
+	      if (map[y][x] > '1' && map[y][x] < '6')
+		kill(enemy_pid(0, 1), SIGUSR1);
+	      else
+		kill(enemy_pid(0, 1), SIGUSR2);
+	      return (0);
+	    }
 	  x1++;
 	}
       y1++;
     }
-  my_printf("missed\n");
 }
